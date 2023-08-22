0c1b7b70c52958a07349757fcb0c0ed920788acd
Srinjan Tambuli <130864418+Srinjan25@users.noreply.github.com>
2023-08-22 21:11:25 +0530

    Add files via upload

---
+++ QuizzitUp!_final/qs1.js
@@ -0,0 +1,76 @@
+// creating an array and passing the number, questions, options, and answers
+let qs = [
+    {
+        numb: 1,
+        question: "Name the Sanskrit poet who is known as the 'Indian Shakespeare'.",
+        answer: "Kalidas",
+        options: [
+            "Tulsidas",
+            "Kautilya",
+            "Sudraka",
+            "Kalidas"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which was the first country to launch 5G network?",
+        answer: "South Korea",
+        options: [
+            "India",
+            "South Korea",
+            "China",
+            "USA"
+        ]
+    },
+    
+    {
+        numb: 3,
+        question: "Who is the 'Half Blood Prince' in the Harry Potter books?",
+        answer: "Severus Snape",
+        options: [
+            "Albus Dumbledore",
+            "Harry Potter",
+            "Severus Snape",
+            "Lord Voldemort"
+        ]
+    },
+    {
+        numb: 4,
+        question: "In the MCU, what is the alias of Sergeant James Buchanan Barnes?",
+        answer: "Winter Soldier",
+        options: [
+            "War Machine",
+            "Winter Soldier",
+            "Daredevil",
+            "Hawkeye"
+        ]
+    },
+        
+    {
+        numb: 5,
+        question: "Which country is set to host the 2022 FIFA World Cup?",
+        answer: "Qatar",
+        options: [
+            "Qatar",
+            "Algeria",
+            "Saudi Arabia",
+            "UAE"
+        ]
+    },
+    
+    
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
---
+++ QuizzitUp!_final/questions.js
@@ -0,0 +1,77 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Who is the first Indian to win gold medal in Olympics?",
+        answer: "Abhinav Bindra",
+        options: [
+            "Abhinav Bindra",
+            "Neeraj Chopra",
+            "KD Jadhav",
+            "Leander Paes"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What was the name of Jon Snow's sword in The Game of Thrones?",
+        answer: "Longclaw",
+        options: [
+            "Heartsbane",
+            "Blackfyre",
+            "Dark Sister",
+            "Longclaw"
+        ]
+    },
+    
+    {
+        numb: 3,
+        question: "Which actor portrayed the role of Thomas Shelby in the show 'Peaky Blinders'?",
+        answer: "Cillian Murphy",
+        options: [
+            "Leonardo DiCaprio",
+            "Benedict Cumberbatch",
+            "Robert Downey Jr.",
+            "Cillian Murphy"
+        ]
+    },
+    
+    {
+        numb: 4,
+        question: "How many MBs make 1GB?",
+        answer: "1024",
+        options: [
+            "1023",
+            "1024",
+            "1026",
+            "1028"
+        ]
+    },
+    
+    {
+        numb: 5,
+        question: "Where was Mahatma Gandhi born in India?",
+        answer: "Porbandar",
+        options: [
+            "Agra",
+            "Ahmedabad",
+            "Gandhinagar",
+            "Porbandar"
+        ]
+    },
+    
+    
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
